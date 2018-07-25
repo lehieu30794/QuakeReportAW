f782ab1db3a138e7742a97bfe5c60b8e867e2ab5
lehieu30794 <lehieu30794@gmail.com>
2018-07-25 11:07:54 -0600

    Create EarthquakeAdapter and list_earthquake_item

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -0,0 +1,43 @@
+package com.example.android.quakereport;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+
+    public EarthquakeAdapter (Activity context, ArrayList<Earthquake> earthquakes){
+
+        super(context, 0, earthquakes);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_earthquake_item,parent, false);
+        }
+
+        Earthquake currentEarthquake = getItem(position);
+
+        TextView magTextView = (TextView) convertView.findViewById(R.id.mag_text_view);
+        magTextView.setText(currentEarthquake.getMag());
+
+        TextView locationTextView = (TextView) convertView.findViewById(R.id.location_text_view);
+        locationTextView.setText(currentEarthquake.getLocation());
+
+        TextView dateTextView = (TextView) convertView.findViewById(R.id.date_text_view);
+        dateTextView.setText(currentEarthquake.getDate());
+
+        return convertView;
+    }
+}
